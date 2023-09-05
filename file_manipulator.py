d47ce5faeafc04c6d2eed1bd6d82f7a237fb6b14
Yasuhiro Nagahama <fnifhubi85h29bddi@gmail.com>
2023-09-05 18:47:06 +0900

    add methods

--- file_manipulator.py
+++ file_manipulator.py
@@ -17,20 +17,26 @@ class FileManipulator:
 
     def reserve():
       input("File Manipulator Program >> ")
-
-    def lengthCheck(self, args):
-       return len(args) == 3 or len(args) == 4
     
-    def commandCheck(self, command):
-       return command in self.commands
+    def is_valid_length(self, args):
+      if args[1] == 'replace-string' and len(args) == 5:
+          return True
+      
+      return len(args) == 4
+    
+    def is_valid_command(self, command):
+      return command in self.commands
 
     def parseInput(self):
       args = sys.argv
 
-      if(self.lengthCheck(args) and self.commandCheck(args[1])):
-         print(args[1])
+      if len(args) <= 1:
+        return print('正しい入力形式ではありません。')
+
+      if(self.is_valid_command(args[1]) and self.is_valid_length(args)):
+        print(args[1])
       else:
-         print('存在しないコマンドです。')
+        print('正しい入力形式でない、もしくは、存在しないコマンドです。')
 
 file_manipurator_program = FileManipulator()
 file_manipurator_program.parseInput()
