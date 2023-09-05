aa21960dd8449b02ed5d60fef860eb2d760ae73b
Yasuhiro Nagahama <fnifhubi85h29bddi@gmail.com>
2023-09-05 12:51:38 +0900

    add class

--- file_manipulator.py
+++ file_manipulator.py
@@ -1,8 +1,38 @@
-# openメソッドでファイルを読み込む
-# closeメソッドでファイルを閉じる（ファイルを閉じることにより、OSはファイル記述子を解放し、ファイルの更新を確定し、他のプロセスで使用できるようにします）
+import sys
+
+class FileManipulator:
+    def __init__(self):
+        self.inputpath = ''
+        self.outputpath = ''
+        self.commands = ['copy', 'reverse', 'duplicate-contents', 'replace-string']
+
+    def description():
+      path_name = 'description.txt'
+      contents = ''
+
+      with open(path_name) as f:
+        contents = f.read()
+
+      print(contents)
+
+    def reserve():
+      input("File Manipulator Program >> ")
+
+    def lengthCheck(self, args):
+       return len(args) == 3 or len(args) == 4
+    
+    def commandCheck(self, command):
+       return command in self.commands
+
+    def parseInput(self):
+      args = sys.argv
+
+      if(self.lengthCheck(args) and self.commandCheck(args[1])):
+         print(args[1])
+      else:
+         print('存在しないコマンドです。')
+
+file_manipurator_program = FileManipulator()
+file_manipurator_program.parseInput()
 
 
-pathname = 'test.txt'
-file = open(pathname)
-contents = file.read()
-file.close()
