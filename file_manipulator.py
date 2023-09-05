dff00ad73950079df2249eaecc51dc2f5f460e1f
YasuhiroNagahama <fnifhubi85h29bddi@gmail.com>
2023-09-05 19:16:52 +0900

    add execute_command

--- file_manipulator.py
+++ file_manipulator.py
@@ -1,44 +1,65 @@
 import sys
 
+
 class FileManipulator:
     def __init__(self):
-        self.inputpath = ''
-        self.outputpath = ''
-        self.commands = ['copy', 'reverse', 'duplicate-contents', 'replace-string']
+        self.input_path = ''
+        self.output_path = ''
+        self.iterations = 0
+        self.new_string = ''
+        self.command = ''
+        self.commands = ['copy', 'reverse',
+                         'duplicate-contents', 'replace-string']
 
-    def description():
-      path_name = 'description.txt'
-      contents = ''
+    # コマンドの説明をコンソールに出力するメソッド
+    def help():
+        path_name = 'help.txt'
+        contents = ''
 
-      with open(path_name) as f:
-        contents = f.read()
+        with open(path_name) as f:
+            contents = f.read()
 
-      print(contents)
+        print(contents)
 
     def reserve():
-      input("File Manipulator Program >> ")
-    
+        print()
+
+    # コマンドが正しい長さか確認してブーリアン値で返すメソッド
     def is_valid_length(self, args):
-      if args[1] == 'replace-string' and len(args) == 5:
-          return True
-      
-      return len(args) == 4
-    
+        if args[1] == 'replace-string' and len(args) == 5:
+            return True
+
+        return len(args) == 4
+
+    # コマンドがcommandsに存在するかブーリアン値で返すメソッド
     def is_valid_command(self, command):
-      return command in self.commands
+        return command in self.commands
 
+    # この関数に来た時点でインデックスが存在しないことはないはず
+    def execute_command(self):
+        if self.command == 'copy':
+            print('reverse')
+        elif self.command == 'reverse':
+            print('copy')
+        elif self.command == 'duplicate-contents':
+            print('duplicate-contents')
+        elif self.command == 'replace-string':
+            print('replace-string')
+
+    # 入力されたコマンドを解析するメソッド
     def parseInput(self):
-      args = sys.argv
+        args = sys.argv
 
-      if len(args) <= 1:
-        return print('正しい入力形式ではありません。')
+        if len(args) <= 1:
+            return print('正しい入力形式ではありません。')
 
-      if(self.is_valid_command(args[1]) and self.is_valid_length(args)):
-        print(args[1])
-      else:
-        print('正しい入力形式でない、もしくは、存在しないコマンドです。')
+        self.command = args[1]
 
-file_manipurator_program = FileManipulator()
-file_manipurator_program.parseInput()
+        if (self.is_valid_command(self.command) and self.is_valid_length(args)):
+            self.execute_command()
+        else:
+            print('正しい入力形式でない、もしくは、存在しないコマンドです。')
 
 
+file_manipulator_program = FileManipulator()
+file_manipulator_program.parseInput()
