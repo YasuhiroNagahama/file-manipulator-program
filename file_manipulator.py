a3f7a7e0641e8eeb9415f1a4f902c05d82dd26f5
YasuhiroNagahama <fnifhubi85h29bddi@gmail.com>
2023-09-08 19:24:04 +0900

    add new class

--- file_manipulator.py
+++ file_manipulator.py
@@ -1,110 +1,136 @@
 import sys
 import os
 
-class FileManipulator:
-    def __init__(self):
-        self.args = []
-        self.input_path = ''
-        self.output_path = ''
-        self.iterations = 0
-        self.needle = ''
-        self.new_string = ''
-        self.command = ''
-        self.command_map = {
-            'copy': self.copy,
-            'help': self.help,
-            'reverse': self.reverse,
-            'duplicate-contents': self.duplicate_contents,
-            'replace-string': self.replace_string
-        }
-        self.contents = ''
+from abc import ABC
 
-    def copy(self):
-        print('copy')
 
-    def reverse(self):
-        print('reverse')
+class CommandInterface(ABC):
+    def set_data(self):
+        pass
 
-    def help(self):
-        path_name = 'help.txt'
-
-        with open(path_name) as f:
-            self.contents = f.read()
-
-        print(self.contents)
-
-    def duplicate_contents(self):
-        # with open(self.input_path, 'r') as f:
-        #   self.contents = f.read()
-
-        # with open(self.input_path, 'w') as f:
-        #   for i in self.iterations:
-        #       f.write(self.contents + self.contents)
-
-        print('duplicate_contents')
-
-    def replace_string(self):
-        print('replace_string')
-
-    # コマンドが正しい長さか確認してブーリアン値で返すメソッド
     def is_valid_length(self):
-        if self.args[1] == 'help':
-            return len(self.args == 2)
-        elif self.args[1] == 'replace-string':
-            return len(self.args) == 5
+        pass
 
-        return len(self.args) == 4
-
-    # コマンドがcommand_mapに存在するかブーリアン値で返すメソッド
-    def is_valid_command(self):
-        return self.command in self.command_map
-
-    # ファイルパスが正しいかブーリアン値で返すメソッド
     def is_valid_path(self, file_path):
+        pass
+
+
+class Help:
+    def __init__(self, args: list):
+        self.valid_length: int = 2
+        self.args: list = args
+
+    def is_valid_length(self):
+        return len(self.args) == self.valid_length
+
+    def run_command(self):
+        if self.is_valid_length():
+            path_name = 'help.txt'
+            contents = ''
+
+            with open(path_name, "r") as f:
+                contents = f.read()
+
+            print(contents)
+        else:
+            print('コマンドの入力形式が間違っています。')
+
+
+class Reverse(CommandInterface):
+    def __init__(self, args: list):
+        self.valid_length: int = 4
+        self.args: list = args
+        self.data_format: str = ''
+        self.input_path: str = ''
+        self.output_path: str = ''
+
+    def is_valid_length(self):
+        return len(self.args) == self.valid_length
+
+    def set_path(self):
+        self.input_path = self.args[2]
+        self.output_path = self.args[3]
+
+    def is_exist_path(self, path):
+        return os.path.exists(path)
+
+    def is_file_path(self, file_path):
         return os.path.isfile(file_path)
 
-    # 現在メンバ変数に保存されているデータが正しいか確認するメソッド
-    def is_valid_data_type(self):
-        if(self.command == 'copy' or self.command == 'reverse'):
-           if self.is_valid_path(self.input_path) and self.is_valid_path(self.output_path):
-               return True
-           else:
-               print("input_path、または、output_pathのパスが正しくありません。")
-               return False
-        elif(self.command == 'duplicate-contents'):
-          try:
-            if not self.is_valid_path(self.input_path):
-                return False
-            
-            n = int(self.iterations)
-            if n > 0:
-                return True
-            else:
-                print('エラー: n を0より大きい正の整数にしてください。')
-                return False
-          except ValueError:
-            print('エラー: n の値が数値に変換できない文字になっています。')
-            return False
+    def run_command(self):
+        if not self.is_valid_length():
+            print('コマンドの入力形式が間違っています。')
+            return
 
-    # 入力されたコマンドから、必要な情報をメンバ変数に保存するメソッド
-    def set_data(self):
-        self.input_path = self.args[2]
+        self.set_path()
 
-        if(self.command == 'copy' or self.command == 'reverse'):
-            self.output_path = self.args[3]
-        elif(self.command == 'duplicate-contents'):
-            self.iterations = self.args[3]
-        elif(self.command == 'replace_string'):
-            self.needle = self.args[3]
-            self.new_string = self.args[4]
+        if not self.is_exist_path(self.input_path):
+            print(self.input_path + 'は存在しません。')
+            return
+        if not self.is_exist_path(self.output_path):
+            print(self.output_path + 'は存在しません。')
+            return
+        
 
-    def analyze_command(self):
-        if self.is_valid_command() and self.is_valid_length():
-            self.set_data()
-            if self.is_valid_data_type():
-                self.command_map[self.command]()
-        else:
-            print('エラー: 正しい入力形式でない、もしくは、存在しないコマンドです。')
+        contents = ''
+
+        with open(self.input_path, "r") as f:
+            file_contents = f.read()
+            contents = file_contents[::-1]
+
+        # 上書きしてもよいか確認する条件分岐
+        # フォルダの場合どうするか？
+        with open(self.output_path, "w") as f:
+            f.write(contents)
+
+        print(contents)
+
+
+class Copy:
+    def __init__(self, args: list):
+        self.args: list = args
+        self.input_path: str = ''
+        self.output_path: str = ''
+
+    def __str__(self):
+        print('copy')
+
+
+class DuplicateContents:
+    def __init__(self, args: list):
+        self.args: list = args
+        self.input_path: str = ''
+        self.iterations = 0
+
+    def __str__(self):
+        print('duplicate contents')
+
+
+class ReplaceString:
+    def __init__(self, args: list):
+        self.args: list = args
+        self.needle = ''
+        self.new_string = ''
+
+    def __str__(self):
+        print('replace string')
+
+
+class FileManipulator():
+    def __init__(self):
+        self.args: list = []
+        self.command: str = ''
+        self.command_class_map: map = {
+            'help': Help,
+            'copy': Copy,
+            'reverse': Reverse,
+            'duplicate-contents': DuplicateContents,
+            'replace-string': ReplaceString
+        }
+
+    # 有効なコマンドかどうかをブーリアン値で返すメソッド
+    def is_exist_command(self):
+        return self.command in self.command_class_map
 
     # 入力されたコマンドを解析するメソッド
     def analyze_input(self):
@@ -115,7 +141,14 @@ class FileManipulator:
             return
 
         self.command = self.args[1]
-        self.analyze_command()
+
+        if not self.is_exist_command():
+            print('エラー: 存在しないコマンドです。')
+            return
+
+        command_program: object = self.command_class_map[self.command](
+            self.args)
+        command_program.run_command()
 
 
 file_manipulator_program = FileManipulator()
